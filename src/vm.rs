bf4ce60a256ee589dc3c06a1b73e92706b902117
svizcaino26 <svizcaino26@gmail.com>
2026-09-03 16:13:38 -0500

    feat: add architecture opcodes and opcode decoder

--- src/vm.rs
+++ src/vm.rs
@@ -59,6 +59,53 @@ pub enum ConditionFlag {
     Neg,
 }
 
+/// Represents the LC-3 instruction set.
+pub enum Opcode {
+    Br = 0,    // branch
+    Add = 1,   // add
+    Ld = 2,    // load
+    St = 3,    // store
+    Jsr = 4,   // jump to subroutine
+    And = 5,   // bitwise and
+    Ldr = 6,   // load register
+    Str = 7,   // store register
+    Rti = 8,   // unused
+    Not = 9,   // bitwise not
+    Ldi = 10,  // load indirect
+    Sti = 11,  // store indirect
+    Jmp = 12,  // jump
+    Res = 13,  // reserved (unused)
+    Lea = 14,  // load effective address
+    Trap = 15, // execute trap
+}
+
+impl From<u16> for Opcode {
+    /// Extracts the opcode field from a 16-bit LC-3 instruction.
+    ///
+    /// The bit shift ensures the matched value is a number between 0 and 15.
+    #[allow(clippy::unreachable)]
+    fn from(value: u16) -> Self {
+        match value >> OPCODE_SHIFT {
+            0 => Self::Br,
+            1 => Self::Add,
+            2 => Self::Ld,
+            3 => Self::St,
+            4 => Self::Jsr,
+            5 => Self::And,
+            6 => Self::Ldr,
+            7 => Self::Str,
+            8 => Self::Rti,
+            9 => Self::Not,
+            10 => Self::Ldi,
+            11 => Self::Sti,
+            12 => Self::Jmp,
+            13 => Self::Res,
+            14 => Self::Lea,
+            15 => Self::Trap,
+            _ => unreachable!(),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,5 +120,12 @@ mod tests {
         assert_eq!(vm.registers.pc, PC_START);
         assert_matches!(vm.registers.cond, ConditionFlag::Zro);
         assert_eq!(vm.registers.general.len(), GENERAL_REGISTERS_SIZE);
+    }
+
+    #[test]
+    fn opcode_conversion() {
+        assert!(matches!(Opcode::from(0x0000), Opcode::Br));
+        assert!(matches!(Opcode::from(0x1000), Opcode::Add));
+        assert!(matches!(Opcode::from(0xF000), Opcode::Trap));
     }
 }
