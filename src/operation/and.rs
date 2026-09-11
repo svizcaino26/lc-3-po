5375e6b6e9cf561d259b35239a85ad747c1b2535
svizcaino26 <svizcaino26@gmail.com>
2026-09-11 17:16:13 -0500

    feat: add unit tests

--- src/operation/and.rs
+++ src/operation/and.rs
@@ -40,3 +40,59 @@ impl BinaryOp for AndOp {
         lhs & rhs
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::assert_matches;
+
+    use crate::{
+        instruction::{DecodedInstruction, RawInstruction},
+        vm::VirtualMachine,
+    };
+
+    use super::*;
+
+    #[test]
+    fn create_and_op() {
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x5FFF));
+
+        let and_op = AndOp::decode(decoded);
+
+        assert!(and_op.is_ok());
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn register_mode_and() {
+        // AND R2, R3, R4
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x54C4));
+        let mut vm = VirtualMachine::default();
+        let dr = Register::R2;
+
+        vm.write_register(Register::R3, 0x0001);
+        vm.write_register(Register::R4, 0x0001);
+
+        let and_op = AndOp::decode(decoded).unwrap();
+        and_op.execute(&mut vm);
+
+        assert_eq!(vm.read_register(dr), 0x0001);
+        assert_matches!(vm.read_cond(), crate::register::ConditionCode::Pos);
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn immediate_mode_and() {
+        // AND R2, R3, #7
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x54E7));
+        let mut vm = VirtualMachine::default();
+        let dr = Register::R2;
+
+        vm.write_register(Register::R3, 0x0001);
+
+        let and_op = AndOp::decode(decoded).unwrap();
+        and_op.execute(&mut vm);
+
+        assert_eq!(vm.read_register(dr), 1);
+        assert_matches!(vm.read_cond(), crate::register::ConditionCode::Pos);
+    }
+}
