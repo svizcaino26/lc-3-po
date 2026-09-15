80b666890134ca53398e95486b21c7895caf767a
svizcaino26 <svizcaino26@gmail.com>
2026-09-15 13:34:25 -0500

    feat: add tests for LDI operation

--- src/operation/ldi.rs
+++ src/operation/ldi.rs
@@ -46,3 +46,79 @@ impl MemoryLoadOp for LdiOp {
         vm.read_memory(Address::from(vm.read_memory(address)))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        instruction::{DecodedInstruction, RawInstruction},
+        register::ConditionCode,
+    };
+
+    use super::*;
+
+    #[test]
+    fn create_ldi_op() {
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x2801));
+
+        let ldi_op = LdiOp::decode(decoded);
+
+        assert!(ldi_op.is_ok());
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn execute_ldi_op() {
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x2801));
+        let mut vm = VirtualMachine::default();
+        let dr = Register::R4;
+        let indirect_address = 0x4000;
+
+        vm.write_memory(vm.read_pc().wrapping_add(0x0001), indirect_address);
+        vm.write_memory(Address::from(indirect_address), 0x0001);
+
+        let ldi_op = LdiOp::decode(decoded).unwrap();
+
+        ldi_op.execute(&mut vm);
+
+        assert_eq!(vm.read_register(dr), 0x0001);
+        assert_eq!(vm.read_cond(), ConditionCode::Pos);
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn execute_ldi_op_zero() {
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x2801));
+        let mut vm = VirtualMachine::default();
+        let dr = Register::R4;
+        let indirect_address = 0x4000;
+
+        vm.write_memory(vm.read_pc().wrapping_add(0x0001), indirect_address);
+        vm.write_memory(Address::from(indirect_address), 0x0000);
+
+        let ldi_op = LdiOp::decode(decoded).unwrap();
+
+        ldi_op.execute(&mut vm);
+
+        assert_eq!(vm.read_register(dr), 0x0000);
+        assert_eq!(vm.read_cond(), ConditionCode::Zro);
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    fn execute_ldi_op_negative() {
+        let decoded = DecodedInstruction::from(RawInstruction::from(0x2801));
+        let mut vm = VirtualMachine::default();
+        let dr = Register::R4;
+        let indirect_address = 0x4000;
+
+        vm.write_memory(vm.read_pc().wrapping_add(0x0001), indirect_address);
+        vm.write_memory(Address::from(indirect_address), 0xFFFF);
+
+        let ldi_op = LdiOp::decode(decoded).unwrap();
+
+        ldi_op.execute(&mut vm);
+
+        assert_eq!(vm.read_register(dr), 0xFFFF);
+        assert_eq!(vm.read_cond(), ConditionCode::Neg);
+    }
+}
