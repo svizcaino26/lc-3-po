e83fe185a11d68b14faf4daedf4629b4c32ff43a
svizcaino26 <svizcaino26@gmail.com>
2026-09-15 23:42:54 -0500

    feat: implement LC-3 STI (store indirect) operation

---
+++ src/operation/sti.rs
@@ -0,0 +1,50 @@
+use crate::{
+    instruction::{DecodedInstruction, InstructionError},
+    memory::Address,
+    operation::{
+        sign_extend, MemoryOp, MemoryOperands, MemoryStoreOp, Offset9, OFFSET_9_BIT_COUNT,
+        PC_OFFSET_9_FIELD,
+    },
+    register::Register,
+    vm::VirtualMachine,
+};
+
+pub struct StiOp {
+    sr: Register,
+    offset: Offset9,
+}
+
+impl MemoryOp for StiOp {
+    type Offset = Offset9;
+
+    fn from_parts(register: Register, offset: Self::Offset) -> Self {
+        Self {
+            sr: register,
+            offset,
+        }
+    }
+
+    fn offset(instruction: &DecodedInstruction) -> Result<Self::Offset, InstructionError> {
+        let offset = instruction.raw().bits(PC_OFFSET_9_FIELD)?;
+        Ok(Offset9(offset))
+    }
+
+    fn execute(self, vm: &mut VirtualMachine) {
+        self.execute_store(vm);
+    }
+}
+
+impl MemoryStoreOp for StiOp {
+    fn operands(self) -> MemoryOperands<Self::Offset> {
+        MemoryOperands {
+            register: self.sr,
+            offset: self.offset,
+        }
+    }
+
+    fn compute_address(offset: Self::Offset, vm: &VirtualMachine) -> crate::memory::Address {
+        let sext_offset = sign_extend(offset.value(), OFFSET_9_BIT_COUNT);
+        let address_indirect = vm.read_pc().wrapping_add(sext_offset);
+        Address::from(vm.read_memory(address_indirect))
+    }
+}
