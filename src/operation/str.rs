3ec5c0b585f86885307867bb8a62776069a5a4fa
svizcaino26 <svizcaino26@gmail.com>
2026-09-16 17:03:11 -0500

    feat: add docs and unit tests

--- src/operation/str.rs
+++ src/operation/str.rs
@@ -9,6 +9,11 @@ use crate::{
     vm::VirtualMachine,
 };
 
+/// Stores the value of a register at an address calculated relative to a base
+/// register.
+///
+/// The address is calculated by sign-extending the encoded 6-bit offset and
+/// adding it to the value of the base register.
 pub struct StrOp {
     sr: Register,
     offset: Offset6,
@@ -50,5 +55,31 @@ impl MemoryStoreOp for StrOp {
     fn compute_address(offset: Self::Offset, vm: &VirtualMachine) -> crate::memory::Address {
         let sext_offset = sign_extend(offset.value(), OFFSET_6_BIT_COUNT);
         Address::from(vm.read_register(offset.base_r()).wrapping_add(sext_offset))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn execute_stores_register_value_at_base_register_relative_address() {
+        let mut vm = VirtualMachine::default();
+        vm.write_register(Register::R1, 0x4002);
+        vm.write_register(Register::R4, 0x4321);
+
+        let offset = Offset6 {
+            base_r: Register::R1,
+            value: 0b11_1110, // -2
+        };
+
+        let op = StrOp {
+            sr: Register::R4,
+            offset,
+        };
+
+        op.execute(&mut vm);
+
+        assert_eq!(vm.read_memory(Address::from(0x4000)), 0x4321);
     }
 }
